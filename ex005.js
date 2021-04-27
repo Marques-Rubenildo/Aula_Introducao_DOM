5fb773ab46b51477dc8f74e481d54a0bdd8b8cd2
Rubenildo Marques <82522946+Marques-Rubenildo@users.noreply.github.com>
2021-04-27 10:21:59 -0300

    novos documentos

--- ex005.js
+++ ex005.js
@@ -1,8 +1,12 @@
-const prompt = require('prompt-sync')();
+var vel = 85
+var cMult = (vel - 60) * 10 //para cada quilômetro que o motorista passar acima do limite de velocidade, este será multiplicado por dez para retornar o valor total da multa.
 
-var vel = Number(prompt('Registrando a velocidade: '));
-console.log(`Sua velocidade é de ${vel}km/h`);
+console.log(`Sua velocidade foi registrada como ${vel}km/h`);
 
+//Analisando os dados registrados pelo sistema para verificar se o motorista ultrapassou o limite de velocidade.
 if (vel > 60) {
-    console.log(`Você ultrapassou o limite de velocidade que é de 60km/h`)
+    console.log(`Você ultrapassou o limite de velocidade que é de 60km/h.`);
+    console.log(`De acordo com o regulamento estadual de trânsito, você será multado em R$${cMult.toFixed(2)}`);
+} else {
+    console.log('Fique tranquilo, sua velocidade está dentro do limite proposto pela lei estadual de trânsito, não esqueça dop cinto de segurança e dirija sempre com cuidado!');
 }
